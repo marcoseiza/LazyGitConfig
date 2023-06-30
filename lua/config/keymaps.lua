45142e040afb8fc523431b4288cfc7bcf36cc573
Marcos Eizayaga <marcoseizayaga@gmail.com>
2023-06-29 20:09:32 -0400

    add terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,7 +3,37 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<leader>/", function()
-  return MiniComment.operator() .. "_"
-end, { expr = true, desc = "Comment line" })
+  if vim.bo.buftype == "" then
+    return MiniComment.operator() .. "_"
+  end
+end, { expr = true, desc = "Comment line", silent = true })
 
 vim.keymap.set("x", "<leader>/", [[:<c-u>lua MiniComment.operator('visual')<cr>]], { desc = "Comment selection" })
+
+vim.keymap.set({ "n", "t" }, "<A-i>", function()
+  require("nvterm.terminal").toggle("float")
+end, { desc = "Open floating terminal" })
+
+vim.keymap.set({ "n", "t" }, "<leader>ft", function()
+  require("nvterm.terminal").toggle("float")
+end, { desc = "Open floating terminal" })
+
+vim.keymap.set({ "n", "t" }, "<c-/>", function()
+  require("nvterm.terminal").toggle("float")
+end, { desc = "Terminal (root dir)" })
+
+vim.keymap.set({ "n", "t" }, "<A-h>", function()
+  require("nvterm.terminal").toggle("horizontal")
+end, { desc = "Open floating terminal" })
+
+vim.keymap.set({ "n", "t" }, "<A-v>", function()
+  require("nvterm.terminal").toggle("vertical")
+end, { desc = "Open floating terminal" })
+
+vim.keymap.set({ "n", "t" }, "<leader>h", function()
+  require("nvterm.terminal").toggle("horizontal")
+end, { desc = "Open floating terminal" })
+
+vim.keymap.set({ "n", "t" }, "<leader>v", function()
+  require("nvterm.terminal").toggle("vertical")
+end, { desc = "Open floating terminal" })
