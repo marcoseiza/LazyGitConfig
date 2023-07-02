93fb29439cd259edaa95f0bdd9eba638d822b13f
Marcos Eizayaga <marcoseizayaga@gmail.com>
2023-07-02 09:23:01 -0400

    add support for fig in terminal

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+if os.execute("which figterm > /dev/null 2>&1") == 0 then
+  vim.fn.setenv("FIG_TERM", nil)
+end
